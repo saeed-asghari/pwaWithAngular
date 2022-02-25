27174a30e0aaddae53dab312963656291c91f6b0
Saeed <saeed.asghari.241@gmail.com>
2022-02-25 22:49:47 +0330

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { SwUpdate } from '@angular/service-worker';
+import { SwPush, SwUpdate } from '@angular/service-worker';
 import { DataService } from './data.service';
 @Component({
   selector: 'app-root',
@@ -10,7 +10,13 @@ export class AppComponent {
   title = 'jokes';
   update: boolean = false;
   joke: any;
-  constructor(updates: SwUpdate, private dataService: DataService) {
+  private readonly publicKey =
+    'BGBAi_YVJceSt_YP1zupzpVSixB96kVthVFXMujq63VaZUNZfFtVobYkez-2jgfWH_9Nf4HvucNNqBmPsMfmfnw';
+  constructor(
+    updates: SwUpdate,
+    private dataService: DataService,
+    private swPush: SwPush
+  ) {
     updates.available.subscribe((event) => {
       this.update = true;
       updates.activateUpdate().then(() => document.location.reload());
@@ -18,8 +24,21 @@ export class AppComponent {
   }
 
   ngOnInit() {
+    this.pushSubscription();
     this.dataService.gimmeJokes().subscribe((res) => {
       this.joke = res;
     });
+  }
+  pushSubscription() {
+    if (!this.swPush.isEnabled) {
+      console.log('Notification is not enabled');
+      return;
+    }
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: this.publicKey,
+      })
+      .then((sub) => console.log(JSON.stringify(sub)))
+      .catch((err) => console.log(err));
   }
 }
